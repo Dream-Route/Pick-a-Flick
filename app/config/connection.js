60855f2e21019ef754ce24a04389f65a81a58994
Mustafa Tunc <mtunc@hotmail.com>
2018-11-26 19:59:17 -0600

    movie adder is almost done

--- app/config/connection.js
+++ app/config/connection.js
@@ -23,38 +23,3 @@ var sequelize = new Sequelize("pickAflick", "root", "", {
 module.exports = sequelize;
 
 
-//////////////////////////////////
-
-// var connection;
-
-// if (process.env.JAWSDB_URL){ 
-
-//   connection= mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-
-//    connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     port: 3306,
-//     password: "",
-//     database: "pickAflick"
-
-
-// });
-
-
-
-// };
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-    
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// // Export connection for our ORM to use.
-// module.exports = connection;
--- app/public/js/view.js
+++ app/public/js/view.js
@@ -1,39 +1,67 @@
+$(document).ready(function () {
+  console.log("ready!");
+  var onLoad = ["The Shawshank Redemption", "The Usual Suspects", "Papillon", "The Terminator"];
+  NewMovieAdder(onLoad)
+  // The createRow function takes data returned by OMDB and appends the table data to the tbody
 
-      // The createRow function takes data returned by OMDB and appends the table data to the tbody
-      var addAmovie=["The Shawshank Redemption","The Usual Suspects","Papillon","The Terminator"];
+  //function NewMovieAdder(){
 
-      var createRow = function(data) {
-        // Get reference to existing tbody element, create a new table row element
-        var tBody = $("tbody");
-        var tRow = $("<tr>");
-       
-  
-        // Methods run on jQuery selectors return the selector they we run on
-        // This is why we can create and save a reference to a td in the same statement we update its text
-        var titleTd = $("<td>").text(data.Title);
-        var ratedTd = $("<td>").text(data.Rated);
-        var runtimeTd = $("<td>").text(data.Runtime);
-        // Append the newly created table data to the table row
-        tRow.append(titleTd, ratedTd, runtimeTd);
-        // Append the table row to the table body
-        tBody.append(tRow);
-      };
-  
+
+  //NewMovieAdder();
+  // clearAndShow();
+
+  $("form").submit(function (event) {
+    event.preventDefault()
+    console.log("adding movie", $(".inputName").val())
+    NewMovieAdder();
+  });
+
+  function NewMovieAdder(array) {
+    var moviiii = $(".inputName").val();
+
+    var createRow = function (data) {
+      // Get reference to existing tbody element, create a new table row element
+      var tBody = $("tbody");
+      var tRow = $("<tr>");
+      // Methods run on jQuery selectors return the selector they we run on
+      // This is why we can create and save a reference to a td in the same statement we update its text
+      var titleTd = $("<td>").text(data.Title);
+      var ratedTd = $("<td>").text(data.Rated);
+      var runtimeTd = $("<td>").text(data.Runtime);
+      // Append the newly created table data to the table row
+      tRow.append(titleTd, ratedTd, runtimeTd);
+      // Append the table row to the table body
+      tBody.append(tRow);
+    };
+
+    var searchOMDB = function (movie) {
+      var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+        createRow(response);
+      });
+    };
+    if (moviiii) { //only run if something is in the text input
+      console.log(moviiii)
+      // onLoad.push(moviiii);
+      searchOMDB(moviiii);
       // The search OMDB function takes a movie, searches the omdb api for it, and then passes the data to createRow
-      var searchOMDB = function(movie) {
-        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-          createRow(response);
-        });
+
+
+    } else if (array) {
+      console.log("Moviii was blank, sending default movies")
+      for (i = 0; i < array.length; i++) {
+
+        searchOMDB(array[i]);
+        // document.getElementById('name-input').innerHTML = addAmovie;
       };
-  
-      // Search the OMDB API for the following movies, and append table rows for each
-      for (i=0; i<addAmovie.length; i++){
-        
-        searchOMDB(addAmovie[i]);
-      
-  }
-  
+    }
+    // Search the OMDB API for the following movies, and append table rows for each
+
+
+  };
+
+
+});
