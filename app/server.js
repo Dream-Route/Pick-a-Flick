383d508cb36f207b540ca92651e1dbc67a05b82b
Mustafa Tunc <mtunc@hotmail.com>
2018-11-26 20:44:32 -0600

    local host

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ var sequelize = require("./config/connection.js");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("app/public"));
+app.use(express.static("public"));
 
 ////////////////////////////////???????????///////
 
